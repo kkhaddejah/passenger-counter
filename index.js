19b7f703d0578e3e3cb8f187b7bbdd3fd2a9d90e
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-02-21 18:23:59 +0400

    Added initial js code.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// document.getElementById("count").innerText = 5;
+let count = 0;
+
+let countEl = document.getElementById("count-el");
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+  console.log(count);
+}
